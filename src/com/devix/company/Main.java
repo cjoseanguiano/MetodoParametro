21eff12626ac94517a1e0db0e39d185c13321f72
Carlos Anguiano <carlos.anguiano@gmail.com>
2017-05-06 17:08:19 -0500

    4.Method with return type and with arguments.

---
+++ src/com/devix/company/Main.java
@@ -0,0 +1,13 @@
+//4.Method with return type and with arguments.
+
+package com.devix.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Sample sample = new Sample();
+        int x = sample.add(1, 2);
+        System.out.println(x);
+    }
+}
---
+++ src/com/devix/company/Sample.java
@@ -0,0 +1,11 @@
+package com.devix.company;
+
+/**
+ * Created by carlosjoseanguiano on 06/05/17.
+ */
+public class Sample {
+    public int add(int a, int b){
+     int c = a + b;
+     return c;
+    }
+}
